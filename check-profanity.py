714b60f8239f5688f839fd093f4f14e1fe809728
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 17:06:56 -0200

    Showing Messages

--- check-profanity.py
+++ check-profanity.py
@@ -10,7 +10,15 @@ def check_profanity(txt_to_check):
     url = "http://www.wdylike.appspot.com/?q=" + (txt_to_check)
     connection = urllib.urlopen(url)
     output = connection.read()
-    print(str(output))
+    show_message(str(output))
+
+def show_message(is_profanity):
+    if "true" in is_profanity:
+        print("There's profanity!")
+    elif "false" in is_profanity:
+        print("It's clean!")
+    else:
+        print("Houston, we have a problem scanning the document :(")
 
 
 read_text()
