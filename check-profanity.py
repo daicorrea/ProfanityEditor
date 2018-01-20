3170aaeba0d832a79b519e3b9dd7d64cbcbc7578
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 16:38:07 -0200

    Calling Google API to Verify Curse Words

--- check-profanity.py
+++ check-profanity.py
@@ -1,6 +1,14 @@
+import urllib.request
+
 def read_text():
     quotes = open("movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
-read_text()
+
+def check_profanity():
+    connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=shit")
+    output = connection.read()
+    print("oi: " + str(output))
+
+check_profanity()
